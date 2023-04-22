a554d7e65b757ba2696a3d4253c14d61c5e20e24
swimchicken <swimchickenOuO@gmail.com>
2023-04-22 14:31:59 +0800

    update 1.0.4 PPD

--- 123.py
+++ 123.py
@@ -10,12 +10,17 @@ df_csv = pd.read_csv("Training Dataset/training datalist.csv")
 print("資料資訊")
 df_csv.info()
 
+#========sex轉成0 or 1============
+
 def replace_sex(x):
     return x - 1
 
-#sex轉成0 or 1
-
 df_csv["Sex"] = df_csv["Sex"].apply(replace_sex)
+
+#========df ppd replace (nan ==> 0)============
+
+df_csv["PPD"].fillna(0, inplace=True)
+
 
 
 
@@ -26,5 +31,5 @@ print("資料label總數(矩陣): ",y.shape)
 
 x = pd.concat([df_csv.iloc[0:2],df_csv[3:26]])
 
-
+print(df_csv["PPD"])
 
